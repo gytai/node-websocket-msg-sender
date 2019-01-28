a6fb80b519a56cee9139c7847f098cded09c9bec
lyb <lyb902832@126.com>
2019-01-28 18:16:28 +0800

    perf:测试问题

--- io/ioHelper.js
+++ io/ioHelper.js
@@ -34,8 +34,8 @@ ioSvc.serverToPrivateMsg = function (uid, data) {
     console.log("sid", sid);
     if (sid && _this.io.sockets.connected[sid]) {
       //给指定的客户端发送消息
-			console.log('_this.io.sockets.sockets', _this.io.sockets.sockets);
-			console.log('_this.io.sockets', _this.io.sockets);
+			console.log('_this.io.sockets.connected[sid]', _this.io.sockets.connected[sid]);
+			//console.log('_this.io.sockets', _this.io.sockets);
       //return console.log('_this.io.sockets.connected', _this.io.sockets.connected);
 			//console.log('_this.io.sockets.connected[sid]', _this.io.sockets.connected[sid]);
 			_this.io.sockets.connected[sid].emit('message', data);
