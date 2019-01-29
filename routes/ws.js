c5227a721553d8eb46e7ea14f15fad7cac887149
lyb <lyb902832@126.com>
2019-01-29 09:43:02 +0800

    perf:代码格式优化

--- routes/ws.js
+++ routes/ws.js
@@ -4,47 +4,47 @@ var ioSvc = require('../io/ioHelper').ioSvc;
 var msgType = require('../io/messageTpye').messageType;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node-Msg-Sender' });
+router.get('/', function (req, res, next) {
+	res.render('index', {title: 'Node-Msg-Sender'});
 });
 
-router.get('/sendMsg', function(req, res, next) {
-    var type = req.query.type || msgType.public;
-    var content = req.query.content || 'none';
-    var uid = req.query.uid;
+router.get('/sendMsg', function (req, res, next) {
+	var type = req.query.type || msgType.public;
+	var content = req.query.content || 'none';
+	var uid = req.query.uid;
 
-    switch (type){
-        case msgType.public:
-            ioSvc.serverBroadcastMsg(content);
-            break;
-        case msgType.private:
-            if(!uid){
-                return res.send({code:400,msg:'uid参数必传'});
-            }
-            ioSvc.serverToPrivateMsg(uid,content);
-            break;
-    }
-    return res.send({code:200,msg:'发送成功'});
-
-});
-
-router.post('/sendMsg', function(req, res, next) {
-	var type = req.body.type || msgType.public;
-	var content = req.body.content || 'none';
-	var uid = req.body.uid;
-
-	switch (type){
+	switch (type) {
 		case msgType.public:
 			ioSvc.serverBroadcastMsg(content);
 			break;
 		case msgType.private:
-			if(!uid){
-				return res.send({code:400,msg:'uid参数必传'});
+			if (!uid) {
+				return res.send({code: 400, msg: 'uid参数必传'});
 			}
-			ioSvc.serverToPrivateMsg(uid,content);
+			ioSvc.serverToPrivateMsg(uid, content);
 			break;
 	}
-	return res.send({code:200,msg:'发送成功'});
+	return res.send({code: 200, msg: '发送成功'});
+
+});
+
+router.post('/sendMsg', function (req, res, next) {
+	var type = req.body.type || msgType.public;
+	var content = req.body.content || 'none';
+	var uid = req.body.uid;
+
+	switch (type) {
+		case msgType.public:
+			ioSvc.serverBroadcastMsg(content);
+			break;
+		case msgType.private:
+			if (!uid) {
+				return res.send({code: 400, msg: 'uid参数必传'});
+			}
+			ioSvc.serverToPrivateMsg(uid, content);
+			break;
+	}
+	return res.send({code: 200, msg: '发送成功'});
 
 });
 
